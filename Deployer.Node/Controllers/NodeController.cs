1ddd3189a2169ccb1c8e29498a210a7a0f655fb1
Antti Järvinen <aj750226@gmail.com>
2020-12-06 19:36:28 +0200

    Add foundation for Deployer.Node

---
+++ Deployer.Node/Controllers/NodeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Deployer.Node.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NodeController : ControllerBase
+    {
+        private readonly ILogger<NodeController> _logger;
+
+        public NodeController(ILogger<NodeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Deploy()
+        {
+            return Ok();
+        }
+    }
+}
